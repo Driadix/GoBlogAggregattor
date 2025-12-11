eb8898a5b6a4457e8cb100d4e826130cc0a30bfb
Driadix <k.ilichev@ozm.ru>
2025-12-11 10:23:38 +0300

    feat: add backround worker that fetches

--- internal/cli/handlers_fetch.go
+++ internal/cli/handlers_fetch.go
@@ -2,10 +2,13 @@ package cli
 
 import (
 	"context"
+	"database/sql"
 	"encoding/xml"
 	"fmt"
+	"gator/internal/database"
 	"html"
 	"net/http"
+	"time"
 )
 
 type RSSFeed struct {
@@ -32,7 +35,9 @@ func fetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
 
 	req.Header.Set("User-Agent", "gator")
 
-	client := &http.Client{}
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
 	resp, err := client.Do(req)
 	if err != nil {
 		return nil, fmt.Errorf("error while fetching feed: %w", err)
@@ -60,12 +65,52 @@ func fetchFeed(ctx context.Context, feedUrl string) (*RSSFeed, error) {
 	return &feed, nil
 }
 
-func HandlerAgg(_ *State, _ Command) error {
-	feed, err := fetchFeed(context.Background(), "https://www.wagslane.dev/index.xml")
-	if err != nil {
-		return err
+func HandlerAgg(s *State, cmd Command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("usage: %s <time_between_reqs>", cmd.Name)
 	}
 
-	fmt.Println(feed)
+	timeBetweenRequests, err := time.ParseDuration(cmd.Args[0])
+	if err != nil {
+		return fmt.Errorf("invalid duration: %w", err)
+	}
+
+	fmt.Printf("Collecting feeds every %s\n", timeBetweenRequests)
+
+	ticker := time.NewTicker(timeBetweenRequests)
+	defer ticker.Stop()
+
+	scrapeFeeds(s)
+
+	for ; ; <-ticker.C {
+		scrapeFeeds(s)
+	}
+}
+
+func scrapeFeeds(s *State) error {
+	feed, err := s.DB.GetNextFeedToFetch(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't get next feed to fetch: %w", err)
+	}
+
+	err = s.DB.MarkFeedFetched(context.Background(), database.MarkFeedFetchedParams{
+		ID:            feed.ID,
+		LastFetchedAt: sql.NullTime{Time: time.Now(), Valid: true},
+	})
+	if err != nil {
+		return fmt.Errorf("couldn't mark feed %s fetched: %w", feed.Name, err)
+	}
+
+	rssFeed, err := fetchFeed(context.Background(), feed.Url)
+	if err != nil {
+		fmt.Printf("Couldn't collect feed %s: %v\n", feed.Name, err)
+		return nil
+	}
+
+	for _, item := range rssFeed.Channel.Item {
+		fmt.Printf("Found post: %s\n", item.Title)
+	}
+	fmt.Printf("Feed %s collected, %v posts found\n", feed.Name, len(rssFeed.Channel.Item))
+
 	return nil
 }
--- sql/queries/feeds.sql
+++ sql/queries/feeds.sql
@@ -1,6 +1,6 @@
 -- name: CreateFeed :one
-INSERT INTO feeds (id, created_at, updated_at, name, url, user_id)
-VALUES ($1,$2,$3,$4,$5,$6)
+INSERT INTO feeds (id, created_at, updated_at, name, url, user_id, last_fetched_at)
+VALUES ($1,$2,$3,$4,$5,$6,$7)
 RETURNING *;
 
 -- name: ListFeeds :many
@@ -11,3 +11,14 @@ JOIN users ON feeds.user_id = users.id;
 -- name: GetFeedByURL :one
 SELECT * FROM feeds
 WHERE feeds.url = $1;
+
+-- name: MarkFeedFetched :exec
+UPDATE feeds
+SET last_fetched_at = $2,
+    updated_at = $2
+WHERE id = $1;
+
+-- name: GetNextFeedToFetch :one
+SELECT * FROM feeds
+ORDER BY last_fetched_at ASC NULLS FIRST
+LIMIT 1;
